edca0134d9f27d2f6312406c9b8e44fb96496cc3
fanrongchen <755042831@qq.com>
2022-03-27 10:11:58 +0800

    添加创建表语句

--- src/main/java/com/example/demo/controller/RunoobTblOneController.java
+++ src/main/java/com/example/demo/controller/RunoobTblOneController.java
@@ -1,7 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.RunoobTblEntity;
+import com.example.demo.entity.RunoobTblOne;
+import com.example.demo.mapper.RunoobTblMapper;
+import com.example.demo.mapper.RunoobTblOneMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Calendar;
 
 /**
  * <p>
@@ -13,6 +21,39 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 @RequestMapping("/runoobTblOne")
+@ResponseBody
 public class RunoobTblOneController {
 
+    @Autowired(required = false)
+    private RunoobTblOneMapper runoobTblOneMapper;
+
+    @RequestMapping("/add")
+    public String add(){
+        RunoobTblOne runoobTblEntity = new RunoobTblOne();
+        runoobTblEntity.setRunoobAuthor("author");
+        runoobTblEntity.setRunoobTitle("tittle");
+        runoobTblOneMapper.insert(runoobTblEntity);
+        return "====";
+    }
+
+    @RequestMapping("/get")
+    public String get(String id){
+        Integer integer = runoobTblOneMapper.checkTableExistsWithSchema("mybatis_plus", "runoob");
+        //        RunoobTblOne oneByRunoobIdAfter = runoobTblOneMapper.getOneByRunoobId(id);
+        return "====";
+    }
+
+    @RequestMapping("/createTable")
+    public String createTable(){
+        Calendar instance = Calendar.getInstance();
+        int year = instance.get(Calendar.YEAR);
+        int month = instance.get(Calendar.MONTH) + 1;
+        int day = instance.get(Calendar.DAY_OF_MONTH);
+        String tableName = "test" + year + "" + month + "" + day;
+        Integer integer = runoobTblOneMapper.checkTableExistsWithSchema("mybatis_plus", tableName);
+        if(integer == 0){
+            runoobTblOneMapper.createNewTableAndInsertData(tableName);        //        RunoobTblOne oneByRunoobIdAfter = runoobTblOneMapper.getOneByRunoobId(id);
+        }
+        return "====";
+    }
 }
--- src/main/java/com/example/demo/mapper/RunoobTblOneMapper.java
+++ src/main/java/com/example/demo/mapper/RunoobTblOneMapper.java
@@ -2,6 +2,9 @@ package com.example.demo.mapper;
 
 import com.example.demo.entity.RunoobTblOne;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -13,4 +16,15 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface RunoobTblOneMapper extends BaseMapper<RunoobTblOne> {
 
+    RunoobTblOne getOneByRunoobId(@Param("runoob_id") String id);
+
+    /**
+     * 使用information_schema检查表是否存在
+     * @param tableSchema
+     * @param tableName
+     * @return
+     */
+    Integer checkTableExistsWithSchema(@Param("tableSchema")String tableSchema, @Param("tableName")String tableName);
+
+    public void createNewTableAndInsertData(@Param("tableName")String tableName);
 }
